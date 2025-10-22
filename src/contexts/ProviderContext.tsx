869312d37d0ed477f13e3bffca2c95b8b45f730f
madaminovsaidaxmad <parolvenom111@gmail.com>
2025-10-22 20:04:30 +0500

    loyiha o'zgartirildi

--- src/contexts/ProviderContext.tsx
+++ src/contexts/ProviderContext.tsx
@@ -1,13 +1,16 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
