7a7bac82a76713c20a980cb12f3b69b192b66e1d
madaminovsaidaxmad <parolvenom111@gmail.com>
2025-10-23 12:07:08 +0500

    finish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import "@/assets/styles/globals.css";
+import Loader from "@/components/Loading";
 import Providers from "@/contexts/ProviderContext"; // 👈 shu import muhim
 import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <Providers> 
-          {children}
-        </Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body>
+                <Providers>
+                    <Loader />
+                    {children}
+                </Providers>
+            </body>
+        </html>
+    );
 }
